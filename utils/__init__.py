8f6edc6873e1f101eb637f886dce17711044cfb1
Hanyu <44048564+Adamli12@users.noreply.github.com>
2024-01-31 22:28:03 +0800

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from CUT.utils.utils import get_model, get_trainer
-from CUT.utils.enum_type import *
-
-__all__ = [
-    'get_model', 'get_trainer', 'Enum', 'ModelType', 'CrossDomainDataLoaderState', 'train_mode2state'
-]
